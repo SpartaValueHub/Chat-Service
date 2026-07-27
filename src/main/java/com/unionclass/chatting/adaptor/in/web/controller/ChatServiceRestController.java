b68c05ad60e5898e7b32fc9e9d5cff9e0ca5d809
jason <beat1103@gmail.com>
2026-07-27 14:21:03 +0900

    fix: chat-service

--- src/main/java/com/unionclass/chatting/adaptor/in/web/controller/ChatServiceRestController.java
+++ src/main/java/com/unionclass/chatting/adaptor/in/web/controller/ChatServiceRestController.java
@@ -1,9 +1,25 @@
 package com.unionclass.chatting.adaptor.in.web.controller;
 
+import com.unionclass.chatting.adaptor.in.web.vo.ChatMessageRequestVo;
+import com.unionclass.chatting.application.port.in.ChatServiceUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/chat")
 @RestController
 public class ChatServiceRestController {
+
+    private final ChatServiceUseCase chatServiceUseCase;
+
+    @PostMapping("/send")
+    public void sendChatMessage(
+            @RequestBody ChatMessageRequestVo chatMessageRequestVo
+    ) {
+        chatServiceUseCase.sendChatMessage();
+    }
+
 }
